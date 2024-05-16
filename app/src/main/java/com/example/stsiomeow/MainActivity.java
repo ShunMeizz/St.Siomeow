8cdb9c6ced8f17266202cdf20b317eb5542b5794
ShunMeizz <sebialshanleyflores@gmail.com>
2024-05-16 09:11:09 +0800

    trying to override

--- app/src/main/java/com/example/stsiomeow/MainActivity.java
+++ app/src/main/java/com/example/stsiomeow/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.stsiomeow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Creates a button that mimics a crash when pressed
-        Button crashButton = new Button(this);
-        crashButton.setText("Test Crash");
-        crashButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                throw new RuntimeException("Test Crash"); // Force a crash
-            }
-        });
-
-        addContentView(crashButton, new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
+        System.out.print("Hello World");
+        System.out.print("Hello World");
     }
 }
